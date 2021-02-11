6f31bb8a514406dda22f0d2b1518d5d7734d49c8
Joshua Eklund <eklun124@umn.edu>
2021-02-11 13:52:47 -0600

    Add code to bare files for todos
    This commit adds the method and code to the todo files so that we can begin to implement the list all todos features. Issue #11
    Co-authored-by: jadynsondrol <sondr037@morris.umn.edu>

--- server/src/main/java/umm3601/todos/Todos.java
+++ server/src/main/java/umm3601/todos/Todos.java
@@ -1,5 +1,11 @@
 package umm3601.todos;
 
-public class todos {
+public class Todos {
+
+  public String _id;
+  public String owner;
+  public boolean status;
+  public String body;
+  public String category;
 
 }
