81ce22d5ff1832bd8a032f4fd0ebbfffe84ea715
Joshua Eklund <eklun124@umn.edu>
2021-02-11 13:37:11 -0600

    Add test for listing all todos
    This commit adds a test for the feature of listing all the todos. Issue #11
    Co-authored-by: jadynsondrol <sondr037@morris.umn.edu>

---
+++ server/src/test/todos/TodosControllerSpec.java
@@ -0,0 +1,57 @@
+package umm3601.todos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import io.javalin.core.validation.Validator;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+import umm3601.Server;
+
+/**
+ * Tests the logic of the UserController
+ *
+ * @throws IOException
+ */
+public class TodosControllerSpec {
+
+  private Context ctx = mock(Context.class);
+
+  private TodosController TodosController;
+  private static TodosDatabase db;
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    ctx.clearCookieStore();
+
+    db = new TodosDatabase(Server.TODOS_DATA_FILE);
+    todosController = new TodosController(db);
+  }
+
+  @Test
+  public void GET_to_request_all_todos() throws IOException {
+    // Call the method on the mock controller
+    todosController.getTodos(ctx);
+
+    // Confirm that `json` was called with all the users.
+    ArgumentCaptor<Todos[]> argument = ArgumentCaptor.forClass(Todos[].class);
+    verify(ctx).json(argument.capture());
+    assertEquals(db.size(), argument.getValue().length);
+  }
+
+}
